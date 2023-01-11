d533fff002596e2659b06f4d06d8e8ed95145d52
Jaime Chavez <jaimechavez23@gmail.com>
2023-01-11 15:57:05 -0600

    updates

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const hiddenFundInput = document.getElementById("hidden-fund-input");
 
 const givingFundInput = document.getElementById("fund-amount");
 
-
 fundList.onchange = function (e) {
-
   let selectedOption = fundList.options[fundList.selectedIndex];
-
 
   hiddenFundInput.name = selectedOption.getAttribute("data-FUND");
   hiddenFundInput.value = selectedOption.value;
   givingFundInput.name = selectedOption.getAttribute("data-GIFT");
+  givingFundInput.id = selectedOption.getAttribute("data-GIFT");
 };
